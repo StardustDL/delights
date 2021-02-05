a59cad3a67ef1f65be5a5cb4a07af251d78fb854
StardustDL <stardustdl@163.com>
2021-02-05 19:30:12 +0800

    fix build retry

--- build/BuildTask.cs
+++ build/BuildTask.cs
@@ -11,6 +11,15 @@ namespace Build
     {
         public override void Run(BuildContext context)
         {
+            try
+            {
+                context.DotNetCoreBuild(Paths.Base.FullPath, new DotNetCoreBuildSettings
+                {
+                    MSBuildSettings = context.GetMSBuildSettings(),
+                });
+            }
+            catch { }
+
             context.DotNetCoreBuild(Paths.Base.FullPath, new DotNetCoreBuildSettings
             {
                 MSBuildSettings = context.GetMSBuildSettings(),
