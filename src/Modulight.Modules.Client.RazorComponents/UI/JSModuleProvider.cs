445a5353e993633bbd9a47dd8a43e50aa830366e
StardustDL <34736356+StardustDL@users.noreply.github.com>
2021-02-20 15:40:35 +0800

    [modulight] Move shared UI to Modulight.UI project. (#39)
    add default blazor ui project
    add global component support #35
    fix default layout collapse
    /pkgs

--- src/Modulight.Modules.Client.RazorComponents/UI/JSModuleProvider.cs
+++ src/Modulight.Modules.Client.RazorComponents/UI/JSModuleProvider.cs
@@ -9,59 +9,6 @@ using System.Threading.Tasks;
 namespace Modulight.Modules.Client.RazorComponents.UI
 {
     /// <summary>
-    /// Specifies <see cref="IRazorComponentClientModule.RootPath"/> for the razor component module.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    public class ModuleUIRootPathAttribute : Attribute
-    {
-        /// <summary>
-        /// Specifies <see cref="IRazorComponentClientModule.RootPath"/> for the razor component module.
-        /// </summary>
-        public ModuleUIRootPathAttribute(string rootPath)
-        {
-            RootPath = rootPath;
-        }
-
-        /// <summary>
-        /// Root path.
-        /// </summary>
-        public string RootPath { get; init; }
-    }
-
-    /// <summary>
-    /// Specifies UI resources.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
-    public class ModuleUIResourceAttribute : Attribute
-    {
-        /// <summary>
-        /// Specifies UI resources.
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="path"></param>
-        public ModuleUIResourceAttribute(UIResourceType type, string path)
-        {
-            Type = type;
-            Path = path;
-        }
-
-        /// <summary>
-        /// Resource type.
-        /// </summary>
-        public UIResourceType Type { get; init; }
-
-        /// <summary>
-        /// Resource relative path.
-        /// </summary>
-        public string Path { get; init; } = string.Empty;
-
-        /// <summary>
-        /// Attributes for the resource.
-        /// </summary>
-        public string[] Attributes { get; init; } = Array.Empty<string>();
-    }
-
-    /// <summary>
     /// Provide JS modules.
     /// </summary>
     /// <typeparam name="T"></typeparam>
---
+++ src/Modulight.Modules.Client.RazorComponents/UI/ModuleUIGlobalComponentAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Modulight.Modules.Client.RazorComponents.UI
+{
+    /// <summary>
+    /// Specifies <see cref="IRazorComponentClientModule.GlobalComponents"/> for the razor component module.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class ModuleUIGlobalComponentAttribute : Attribute
+    {
+        /// <summary>
+        /// Specifies <see cref="IRazorComponentClientModule.GlobalComponents"/> for the razor component module.
+        /// </summary>
+        public ModuleUIGlobalComponentAttribute(Type type)
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Root path.
+        /// </summary>
+        public Type Type { get; init; }
+    }
+}
